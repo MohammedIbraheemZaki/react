1954b4472222501217e3d11d4a9693215ff76d15
MohammedIbraheemZaki <zakiz0054@gmail.com>
2022-01-08 14:42:24 +0200

    1- edits

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react/cjs/react.development';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Person from './Person'
 import Developers from './Developers'
